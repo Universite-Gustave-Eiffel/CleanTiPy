cbbda8a185dcc932430fec44b005d9551bdd14d3
to156205 <nicolas.aujogue@airbus.com>
2025-09-09 12:32:49 +0200

    initialize file where INSA will work

---
+++ cleantipy/Demodulation.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+CLEAN-T demodulated core functions
+
+Created on Tue Sep 09 2025
+@author: Naujogue
+
+
+for gathering the requirements :
+run "pipreqs ." in a terminal running in the current folder
+"""
+
